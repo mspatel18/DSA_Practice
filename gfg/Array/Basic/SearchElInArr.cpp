c98f8004347d130c6d1b3fb495eeb896ace7eecd
Mann Patel <mannpatel3118@gmail.com>
2022-08-18 23:45:34 +0530

    18-08

--- gfg/Array/Basic/SearchElInArr.cpp
+++ gfg/Array/Basic/SearchElInArr.cpp
@@ -1,28 +1,29 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-
 // } Driver Code Ends
-class Solution{
-    public:
+class Solution
+{
+public:
     // Function to search x in arr
     // arr: input array
     // X: element to be searched for
     int search(int arr[], int N, int X)
     {
-        
+
         // Your code here
-        int no=-1;
-        for(int i=0;i<N;i++){
-            if(arr[i]==X){
-                no=i;
+        int no = -1;
+        for (int i = 0; i < N; i++)
+        {
+            if (arr[i] == X)
+            {
+                no = i;
                 break;
             }
         }
         return no;
     }
-
 };
 
 //{ Driver Code Starts.
@@ -30,25 +31,24 @@ class Solution{
 int main()
 {
     int testcases;
-    cin>>testcases;
-    while(testcases--)
+    cin >> testcases;
+    while (testcases--)
     {
         int sizeOfArray;
-        cin>>sizeOfArray;
+        cin >> sizeOfArray;
         int arr[sizeOfArray];
         int x;
-        
-        for(int i=0;i<sizeOfArray;i++)
-        {
-            cin>>arr[i];
-        }
-        cin>>x;
-        Solution ob;
-        cout<<ob.search(arr,sizeOfArray,x)<<endl; //Linear search
-    }
 
-    return 0;
+        for (int i = 0; i < sizeOfArray; i++)
+        {
+            cin >> arr[i];
+        }
+        cin >> x;
+        Solution ob;
+        cout << ob.search(arr, sizeOfArray, x) << endl; // Linear search
+    }
     
+    return 0;
 }
 
 // } Driver Code Ends
