f7fedfc4879c3a1bed0578570ef3b7ac0b7955f0
Mann Patel <mannpatel3118@gmail.com>
2022-09-01 23:36:04 +0530

    diff 350

---
+++ CodeChef/SEMCOURSES.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	for (int i = 0; i < t ; i++) {
+	    int x,y,z;
+	        cin>>x>>y>>z;
+	        cout<<x*y*z<<endl;
+	}
+	return 0;
+}
