873ca8e80de7a45325a8b04be1e9f38487dc24ab
Mann Patel <mannpatel3118@gmail.com>
2022-09-09 22:19:18 +0530

    float while divide

---
+++ CodeChef/Contests/Sept22Long/SPEEDTEST.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	for (int i = 0; i < t; i++) {
+	    float a,x,b,y;
+	    cin>>a>>x>>b>>y;
+	    if((a/x) > (b/y))
+	        cout<<"Alice"<<endl;
+	    else if((a/x)<(b/y))
+	        cout<<"Bob"<<endl;
+	    else
+	        cout<<"Equal"<<endl;
+	}
+	return 0;
+}
