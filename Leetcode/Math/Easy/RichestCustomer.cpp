9d679c9c4d7072c13e00e0e04d8ac177cb87a322
Mann Patel <mannpatel3118@gmail.com>
2022-09-28 23:41:43 +0530

    #1672 max element

---
+++ Leetcode/Math/Easy/RichestCustomer.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        vector<int>ans;
+        for(int i=0;i<accounts.size();i++){
+            int sum=0;
+            for(int j=0;j<accounts[i].size();j++){
+                sum+=accounts[i][j];
+            }
+            ans.push_back(sum);
+        }
+        return *max_element(ans.begin(),ans.end());
+    }
+};
