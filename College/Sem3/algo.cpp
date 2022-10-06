a5d4a2c7ffdd871b60c035d9a25aa1a1ca4dddfe
Mann Patel <mannpatel3118@gmail.com>
2022-10-06 23:01:04 +0530

    edit

---
+++ College/Sem3/algo.cpp
@@ -0,0 +1,22 @@
+//push algo or enqueue algo
+// start
+// if rear=max-1;
+// then queue overflow
+// if front =-1 && rear =-1
+// then f=0 r=0;
+// queue[rear]=num
+// exit
+//
+//start
+//delete or dequeue algo
+//start
+// if front=-1 or front>rear
+// then queue underflow
+// if front == rear
+// front = rear=-1
+// else f=f+1
+// y=queue[front]
+// front = front + 1;
+// return y;
+//exit
+
